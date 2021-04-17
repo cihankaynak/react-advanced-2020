e09ccce3f35ad35219b2e4660d4bf966a66cde5a
Cihan Kaynak <cihan.kaynak@cerner.com>
2021-04-17 09:18:06 -0500

    Completed using custom hooks.

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,30 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './2-useFetch'
+import React from "react";
+import { useFetch } from "./2-useFetch";
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = "https://course-api.com/javascript-store-products";
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
-  console.log(products)
+  const { loading, products } = useFetch(url);
   return (
     <div>
-      <h2>{loading ? 'loading...' : 'data'}</h2>
+      <h2>{loading ? "loading..." : "data"}</h2>
+      {!loading &&
+        products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Example
+const Product = ({ product }) => {
+  console.log(product);
+  return <img src={product.fields.image[0].url} alt="no image" />;
+};
+
+export default Example;
